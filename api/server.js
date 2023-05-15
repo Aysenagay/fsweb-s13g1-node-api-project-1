39dfce8ab7aa0a6d148f43ac37458b9510563ece
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-15 17:29:35 +0300

    GET ve POST yapıldı.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,58 @@
 // SUNUCUYU BU DOSYAYA KURUN
 const express = require("express");
+const Users = require("./users/model");
 const server = express();
-
+server.use(express.json());
 server.get("/", (req, res) => {
   res.send("Server is up and running!...");
 });
+
+//POST
+server.post("/api/users", (req, res) => {
+  let user = req.body;
+  if (!user.name || !user.bio) {
+    res
+      .status(400)
+      .json({ message: "Lütfen kullanıcı için bir name ve bio sağlayın" });
+  } else {
+    Users.insert(user)
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ message: "Veritabanına kaydedilirken bir hata oluştu" });
+      });
+  }
+});
+
+//GET
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Kullanıcı bilgileri alınamadı" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "Belirtilen ID'li kullanıcı bulunamadı" });
+      }
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Kullanıcı bilgisi alınamadı" });
+    });
+});
+
+//DELETE
 
 module.exports = server; // SERVERINIZI EXPORT EDİN {}
